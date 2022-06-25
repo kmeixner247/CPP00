0603d07c14219aad274ed35bbbb7bff2fc20d01f
Konstantin Meixner <konstantin.meixner@freenet.de>
2022-06-25 10:40:22 +0200

    megaphone

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmeixner <konstantin.meixner@freenet.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 17:25:06 by kmeixner          #+#    #+#             */
+/*   Updated: 2022/06/25 10:39:48 by kmeixner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	std::string test;
+
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				argv[i][j] = std::toupper(argv[i][j]);
+			std::cout << argv[i];
+		}
+		std::cout << std::endl;
+	}
+}
